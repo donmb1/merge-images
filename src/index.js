5c0b27d5e81142a1ddee9f740faddb988bf17c90
Martin Braun <martinbr83@gmail.com>
2020-02-25 22:36:08 +0100

    add cross origin anonymous

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ const mergeImages = (sources = [], options = {}) => new Promise(resolve => {
 
 		// Resolve source and img when loaded
 		const img = new Image();
+		img.crossOrigin = "Anonymous";
 		img.onerror = () => reject(new Error('Couldn\'t load image'));
 		img.onload = () => resolve(Object.assign({}, source, { img }));
 		img.src = source.src;
